7983770cd236309f16743db71debf24cc0016bab
mjdufort <matt.dufort@gmail.com>
2019-10-24 12:05:11 -0700

    Patch bug in prev_visit_at_threshold argument checks

--- R/prev_visit_at_threshold.R
+++ R/prev_visit_at_threshold.R
@@ -1,11 +1,15 @@
 #' Determine which visits for any subject occur after C-peptide AUC has reached the detection limit
 #'
-#' This function determines which visits (if any) for each subject occur after that subject's C-peptide AUC has
-#' reached the detection limit. For AUC values, this means that the C-peptide levels for each timepoint are at the
-#' detection limit; the AUC detection limit should be calculated as such. As input, the function takes a data frame of
-#' C-peptide values, with subject identifiers and a date or visit column on which to sort the visits. Note that
-#' visits will be sorted blindly, so character visit names (e.g. Day 1, Week 1, Month 2) should be used with EXTREME
-#' CAUTION! Numeric, date, or factor visit values are strongly preferred.
+#' This function determines which visits (if any) for each subject occur after that subject's
+#' C-peptide AUC has reached the detection limit. This function is intended to be used to filter
+#' out such visits, to prevent violations of the linearity assumption in the model fitting for
+#' slope estimation. For AUC values, it's assumed that the AUC has reached the detection limit
+#' when means the C-peptide levels for each timepoint in the MMTT or OGTT are at the detection 
+#' limit; the AUC detection limit should be calculated as such. As input, the
+#' function takes a data frame of C-peptide values, with subject identifiers and a date or visit
+#' column on which to sort the visits. Note that visits will be sorted blindly, so character visit
+#' names (e.g. Day 1, Week 1, Month 2) should be used with EXTREME CAUTION! Numeric, date, or
+#' factor visit values are strongly preferred.
 #' @param cpeptide_auc_data data frame containing the C-peptide data. Should contain a unique subject identifer, a column for sorting visits, and the C-peptide AUC data. Each row should be a single visit.
 #' @param threshold numeric, the threshold C-peptide AUC value. Values at or below this level will be treated as sub-threshold.
 #' @param identifier_column character or numeric, the column containing the subject identifiers. By default it uses "subject".
@@ -23,14 +27,32 @@ prev_visit_at_threshold <-
            identifier_column="subject", sort_column="visit",
            auc_column="auc") {
     
-    # ensure character column names (necessary for dplyr functions)
-    if (is.numeric(identifier_column)) identifier_column <- colnames(cpeptide_auc_data)[identifier_column]
-    if (is.numeric(sort_column)) sort_column <- colnames(cpeptide_auc_data)[sort_column]
-    if (is.numeric(auc_column)) auc_column <- colnames(cpeptide_auc_data)[auc_column]
+    # ensure character column names (necessary for dplyr functions), and that they are present in data
+    if (is.numeric(identifier_column)) {
+      if (!(identifier_column %in% 1:ncol(cpeptide_auc_data)))
+        stop("Argument `identifier_column` specifies a column number that is not present in input data.")
+      identifier_column <- colnames(cpeptide_auc_data)[identifier_column]
+    } else if (!(identifier_column %in% colnames(cpeptide_auc_data)))
+      stop("Argument `identifier_column` specifies a column name that is not present in input data.")
+      
+    if (is.numeric(sort_column)) {
+      if (!(sort_column %in% 1:ncol(cpeptide_auc_data)))
+        stop("Argument `sort_column` specifies a column number that is not present in input data.")
+      sort_column <- colnames(cpeptide_auc_data)[sort_column]
+    } else if (!(sort_column %in% colnames(cpeptide_auc_data)))
+      stop("Argument `sort_column` specifies a column name that is not present in input data.")
+    
+    if (is.numeric(auc_column)) {
+      if (!(auc_column %in% 1:ncol(cpeptide_auc_data)))
+        stop("Argument `auc_column` specifies a column number that is not present in input data.")
+      auc_column <- colnames(cpeptide_auc_data)[auc_column]
+    } else if (!(auc_column %in% colnames(cpeptide_auc_data)))
+      stop("Argument `auc_column` specifies a column name that is not present in input data.")
     
     cpeptide_auc_data$orig_row <- 1:nrow(cpeptide_auc_data)
     
-    if (!is.null(sort_column)) cpeptide_auc_data <- dplyr::arrange(cpeptide_auc_data, !! rlang::sym(sort_column))
+    if (!is.null(sort_column))
+      cpeptide_auc_data <- dplyr::arrange(cpeptide_auc_data, !! rlang::sym(sort_column))
 
     cpeptide_auc_data$prev_visit_at_threshold <- FALSE
     
